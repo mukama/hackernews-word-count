a85aee0561a9da6015ecc11d863d366356369416
Caesar Mukama <>
2019-10-12 02:10:18 +0300

    hacker news service refactor

--- routes/api.js
+++ routes/api.js
@@ -3,41 +3,13 @@ var router = express.Router();
 var hackernews = require('../services/hackernews')
 
 router.get('/common-in-titles', async (req, res, next) => {
-  const { limit, max } = req.query;
-  const recent = await hackernews.getRecent(max);
-  const stories = await hackernews.getTitles(recent);
-  const titles = stories
-    .map(title => title)
-    .reduce((a, b) => a.concat(b, ' '), '')
-    .replace(/[^0-9a-z-A-Z ]/g, "").replace(/ +/, " ")
-    .toLowerCase()
-    .split(' ');
-
-  // count
-  const countedWords = titles.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
-    return acc;
-  }, {});
-
-  // sort
-  const sortedWords = Object.keys(countedWords)
-    .sort(function (a, b) { return countedWords[b] - countedWords[a] })
-    .filter((key) => key)
-    .slice(0, limit);
-
-  // then filter
-  const filtered = Object.keys(countedWords)
-    .filter(key => sortedWords.includes(key))
-    .reduce((obj, key) => {
-      obj[key] = countedWords[key];
-      return obj;
-    }, {});
-
-  res.json(filtered)
+  const common = await hackernews.commonWords('story', 25, 250);
+  res.json(common)
 });
 
 router.get('/common-in-comments', async (req, res, next) => {
-  res.send('respond with a resource');
+  const common = await hackernews.commonWords('comment', 25, 1000, true, true);
+  res.json(common)
 });
 
 module.exports = router;
--- services/hackernews.js
+++ services/hackernews.js
@@ -2,16 +2,72 @@ var axios = require('axios');
 
 const ENDPOINT = 'https://hacker-news.firebaseio.com/v0'
 const hackernews = {
-  getRecent(limit) {
+  getStoryIds() {
     return axios.get(`${ENDPOINT}/newstories.json?print=pretty`)
-      .then(response => response.data.slice(0, limit));
+      .then(response => response.data);
   },
-  async getTitles(ids) {
+  async getItems() {
+    const ids = await this.getStoryIds();
     return await Promise.all(ids.map(id => {
       return axios.get(`${ENDPOINT}/item/${id}.json?print=pretty`)
-        .then(response => response.data.title)
+        .then(response => response.data)
         .catch(() => '');
     }));
+  },
+  async getKids(max) {
+    const items = await this.getItems();
+    let ids = [];
+    for (let i = 0; (ids.length >= (max || Infinity)) || (i < items.length); i++) {
+      if (items[i] && items[i].kids) {
+        ids.push(...items[i].kids);
+      }
+    }
+
+    console.log(ids)
+
+    return await Promise.all(ids.map(id => {
+      return axios.get(`${ENDPOINT}/item/${id}.json?print=pretty`)
+        .then(response => response.data)
+        .catch(() => '');
+    }));
+  },
+  async commonWords(property, limit, max, traverseKids, inText) {
+    let items;
+    if (traverseKids) {
+      items = await this.getKids();
+    } else {
+      items = await this.getItems();
+    }
+    const allWords = items
+      .filter(a => a && a.type === property)
+      .slice(0, max)
+      .map(a => inText ? a.text : a.title)
+      .reduce((a, b) => a.concat(b, ' '), '')
+      .replace(/[^0-9a-z-A-Z ]/g, "").replace(/ +/, " ")
+      .toLowerCase()
+      .split(' ');
+
+    // count
+    const countedWords = allWords.reduce((acc, curr) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    }, {});
+
+    // sort
+    const sortedWords = Object.keys(countedWords)
+      .sort(function (a, b) { return countedWords[b] - countedWords[a] })
+      .filter((key) => key)
+      .slice(0, limit);
+
+    // then filter
+    const filtered = Object.keys(countedWords)
+      .filter(key => sortedWords.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = countedWords[key];
+        return obj;
+      }, {});
+
+    return filtered;
   }
 }
 
